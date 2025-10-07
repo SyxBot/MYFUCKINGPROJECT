91b420bc025835c01aaf2c062f4b3c93c6ed698b
SyxBot <syxisonfire@gmail.com>
2025-10-07 21:58:05 +0000

    Update entire project

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  walletAddress: '0xf909a79D938A79F0D668E765736d56c3aD4b6397',
-  rpcUrl: process.env.RPC_URL || '', // you set this in the Replit secret
-  telegram: {
-    token: process.env.TELEGRAM_BOT_TOKEN || '',
-    chatId: process.env.TELEGRAM_CHAT_ID || '',
-  },
-};
